d1fd3e8519d2ccec6a50e652d96d47a11a38ba54
MAR2807 <105993122+MAR2807@users.noreply.github.com>
2022-10-17 18:09:08 -0400

    new-oct17-2022

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,11 @@
 import { motion } from 'framer-motion';
 import { useRef } from 'react';
 import notes from './images/notes.png';
-import weather from './images/weather-app.png';
+import weather from './images/weather2.png';
 import portfolio from './images/portfolio.png';
 import abc1 from './images/abc1.png';
 import resume1 from './images/resume1.pdf';
+import tictactoe10 from './images/tictactoe10.png';
 
 export default function Home() {
   const projects = useRef(null);
@@ -60,32 +61,77 @@ export default function Home() {
     <motion.div  onClick={() => scrollToSection(projects)} className = "down_arrow"> <motion.img initial={{opacity:0, y:10}} animate = {{ y:80, opacity:1}}  transition={{duration:1, yoyo:Infinity, delay:4}}  className="down_arrow" src ="http://visualeyespdx.com/wp-content/uploads/2015/08/vec_down_arrow.png" alt ="down_Arrow"></motion.img>
         
     </motion.div>
-
+    <div className='mainContainer'>
       <div className="projects" ref={projects}>
-     <h1 className='projectHead'>Projects</h1>  
-        <div className="project1"> <a href="https://noteify-app-c0702d.netlify.app/" target="_blank" rel="noreferrer"> <button className="projectLinks"> Notes App: live website </button></a>
-        <a href="https://github.com/MAR2807/notes-app" target="_blank" rel="noreferrer"> <button className="projectLinks"> Notes App: Github </button></a>
+     
+        <div className="project1"> 
+       <div className='project_title_1'> <h1 className='project_title_1'>Projects</h1> </div>
+        <div className='info_p1'>
+          <h1> Notes App</h1>
+            <a href="https://noteify-app-c0702d.netlify.app/" target="_blank" rel="noreferrer"> <button className="projectLinks1"> Notes App: live website </button></a>
+            <a href="https://github.com/MAR2807/notes-app" target="_blank" rel="noreferrer"> <button className="projectLinks1"> Notes App: Github </button></a>
             <p> A notes app made using firebase and firestore api from google. Has features that allow users to sign up, sign in and upload notes which can be viewed from anywhere.(Authentication and API) </p>
+        </div>
+        <div className='p1_container'>
+          <div className='img_p1_1'>
             <img src={notes} className='notes-image' alt = "notes-app"></img>
+          </div>
+            
         </div>
-        <div className="project2"> <a href="https://github.com/MAR2807/weather-app" target="_blank" rel="noreferrer"> <button className="projectLinks"> Weather App: Github</button></a>
-        <p> My first project, a simple weather app using an api to pull data for a given city. Uses openweathermap API </p>
-        <img src={weather} className='notes-image' alt = "weather App"></img>
         </div>
-        <div className="project3"> <a href="https://mohamedrafeek.netlify.app/" target="_blank" rel="noreferrer"> <button className="projectLinks"> My portfolio </button></a>
-        <p> This website is one of my recent projects. It was built to show my projects and resume.</p>
-        <img src={portfolio} className='portfolio' alt = "portfolio"></img>
+          
+        <div className="project2">
+            <div className='info_p2'>
+            <h1> Weather App</h1>
+               <a href="https://github.com/MAR2807/weather-app" target="_blank" rel="noreferrer"> <button className="projectLinks1"> Weather App: Github</button></a>
+            <p> My first project, a simple weather app using an api to pull data for a given city. Uses openweathermap API </p>
+          </div>
+          <div className='p2_container'>
+          <div className='img_p2_2'>
+            <img src={weather} className='weather-image' alt = "weather App"></img>
+            </div>
+            </div>
+
+        </div>
+
+        <div className="project3"> 
+        <div className='info_p3'>
+        <h1> Portfolio </h1>
+            <a href="https://mohamedrafeek.netlify.app/" target="_blank" rel="noreferrer"> <button className="projectLinks1"> My portfolio </button></a>
+            <p> This website is one of my recent projects. It was built to show my projects and resume.</p>
+        </div>
+        <div className='p3_container'>
+        <div className='img_p3_3'>
+        <img src={portfolio} className='portfolio1' alt = "portfolio"></img>
+        </div>
+        </div>
+        </div>
+
+        <div className="project4"> 
+        <div className='info_p4'>
+        <h1> Tictactoe </h1>
+            <a href="https://github.com/MAR2807/tictactoe" target="_blank" rel="noreferrer"> <button className="projectLinks1"> Github </button></a>
+            <p> A tictactoe game I made using Javascript and React</p>
+        </div>
+        <div className='p4_container'>
+        <div className='img_p4_4'>
+        <img src={tictactoe10} className='tictactoe' alt = "tictactoe"></img>
+        </div>
+        </div>
+        
         </div>
         
 
       </div>
        <div className="about" ref={about}> 
+       <div className='about-title'><h1 className='about-title'>About </h1></div>
           <div className='about-info'>
-            <h1>About </h1>
-            <p>   My name is Mohamed. I graduated from Edison Highschool as class of 2019 and I am currently attending 
-                   Middlesex County College to complete my associates degree for computer science. 
-                   I have completed most of my requirements and hope to be graduating in less than 3 months. I was born in Sri Lanka and I moved to America in 2006 and 
-                   I've been living in New Jersey ever since. I grew up in Fords,NJ and by the time I was in middle school I moved to Edison. Building Pc's is also one of my hobbies, I like to tinker with parts and upgrade them every now and then. 
+            
+            <p className = "about-info">   
+                  Hi, my name is Mohamed. I graduated from Edison Highschool as class of 2019 and I am currently attending 
+                   Middlesex College to complete my associates degree for computer science. 
+                   I have completed most of my requirements and hope to be graduating this december with an associates in computer science. I prefer to 
+                   work with Javascript using the React framework but I am also proficient in Java, PHP, and SQL.
             </p>
           </div>
        
@@ -101,17 +147,17 @@ export default function Home() {
            </div>
        </div>
        <div className="resume" ref={resume}> 
-       <div className="resumeTitle"><h1>Resume</h1></div>
+       <div className="resumeTitle"><h1 className='resumeTitle'>Resume</h1></div>
           <div className='resumeContainer'>
-           <div className='resume-img-container'>
+          
               <iframe className='resumePDF' src={resume1} ></iframe>
-          </div>
+        
            
           </div> 
 
         
        </div>
- 
+       </div>
     
 </div>
   )
